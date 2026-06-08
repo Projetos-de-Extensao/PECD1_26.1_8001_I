44d8c28da0710a4d11f0f9bb387e42f2d44c13e2
130nardo <202508560348@alunos.ibmec.edu.br>
2026-06-08 10:53:23 -0300

    componente footer

---
+++ src/aac/aac/src/components/footer.jsx
@@ -0,0 +1,10 @@
+function Footer() { 
+  return (
+    <>
+    <footer>
+      SIA Ibmec · Projeto em Ciência de Dados I — 2026.1 · Bernardo, Carlos, Julia, Leonardo, Matheus e Pietro
+    </footer>
+    </>
+  );
+}
+export default Footer;
